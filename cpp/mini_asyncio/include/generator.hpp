b9cdd1aa41f9afd816f47fbff43ca1e3db523dcc
MacDue <macdue@dueutil.tech>
2022-04-16 23:34:19 +0100

    WIP: Remove exception-based control flow

--- cpp/mini_asyncio/include/generator.hpp
+++ cpp/mini_asyncio/include/generator.hpp
@@ -1,31 +1,49 @@
 #pragma once
 
-#include <exception>
+#include <optional>
 
-struct StopIteration : public std::exception {
-  const char* what() const noexcept {
-    return "StopIteration";
-  }
+struct StopIteration {};
+
+template<typename TResult>
+struct GeneratorResult {
+  GeneratorResult(StopIteration) {};
+  GeneratorResult(TResult result)
+    : result{result}{};
+  bool stop_iteration() const { return !result.has_value(); }
+  TResult& operator*() { return *result; }
+private:
+  std::optional<TResult> result;
+};
+
+template<>
+struct GeneratorResult<void> {
+  GeneratorResult() = default;
+  GeneratorResult(StopIteration)
+    : m_stop{true}{};
+  bool stop_iteration() const { return m_stop; }
+  void operator*() { return; }
+private:
+  bool m_stop = false;
 };
 
 #define TOKENPASTE(x, y) x ## y
 #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 
 #define yeild(value) \
-  TOKENPASTE2(_yield, __LINE__): if (_rentry != &&TOKENPASTE2(_yield, __LINE__)) { _rentry = &&TOKENPASTE2(_yield, __LINE__); return (value); } \
+  TOKENPASTE2(_yield, __LINE__): if (_rentry != &&TOKENPASTE2(_yield, __LINE__)) { _rentry = &&TOKENPASTE2(_yield, __LINE__); return GeneratorResult(value); } \
   else { _rentry = nullptr; }
 
-#define yield_from(gen)           \
-  while (true) {                  \
-    try {                         \
-      yeild((gen)());             \
-    } catch (StopIteration) {     \
-      break;                      \
-    }                             \
+#define yield_from(gen)             \
+  while (true) {                    \
+    auto result = (gen)();          \
+    if (result.stop_iteration()) {  \
+      break;                        \
+    }                               \
+    yeild(*result);                 \
   }
 
 #define greturn \
-  {throw StopIteration{};}
+  {GeneratorResult(StopIteration{});}
 
 #define make_generator(body)       \
   ({                               \
